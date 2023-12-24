462a1c8f81ed8876257dfd934b4c6a87fa1585b2
Nodazi <dk3146@naver.com>
2023-12-24 20:46:17 +0900

    Added brief

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -1,5 +1,14 @@
-// Copyright (c) 2023 Nodazi, All Rights Reserves
-// Author : DaHyun Noh
+/**
+ *@file geometry/point2d.cpp
+ *@author DaHyun Noh (dk3146@naver.com)
+ *@brief Point class developments with 2-dimension
+ *@version 1.0.0
+ *@date 2023-12-20
+ *@copyright Copyright (c) 2023 Programmers, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors : DaHyun Noh
 
 #include "geometry/point2d.hpp"
 
