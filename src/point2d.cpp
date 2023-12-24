5db1ad541db40fc7b099b6ce8cad5f89fe6740ff
Nodazi <dk3146@naver.com>
2023-12-24 23:05:16 +0900

    Added functions,

--- src/point2d.cpp
+++ src/point2d.cpp
@@ -22,7 +22,21 @@ namespace nodazi::geometry {
 Point2D::Point2D(double input_x, double input_y) : x_(input_x), y_(input_y){};
 
 auto Point2D::CalculateDistance(const Point2D& target) const -> double {
-  return std::sqrt(std::pow((x_ - target.x_), 2) + std::pow(y_ - target.y_, 2));
+  return Point2D::CalculateDistance(*this, target);
 }
+
+auto Point2D::CalculateDistance(const Point2D& lhs,
+                                const Point2D& rhs) -> double {
+  return std::sqrt(std::pow((lhs.x_ - rhs.x_), 2) +
+                   std::pow(lhs.y_ - rhs.y_, 2));
+}
+
+auto Point2D::GetX() const -> double { return x_; }
+
+auto Point2D::GetY() const -> double { return y_; }
+
+auto Point2D::SetX(double input_x) -> void { x_ = input_x; }
+
+auto Point2D::SetY(double input_y) -> void { y_ = input_y; }
 
 }  // namespace nodazi::geometry
