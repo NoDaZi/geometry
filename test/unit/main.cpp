93c348bafee3dd8c54c2ddb9e270c1b64e498507
Nodazi <dk3146@naver.com>
2023-12-24 20:32:23 +0900

    Added Main test code

---
+++ test/unit/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Nodazi, All Rights Reserves
+// Author : DaHyun Noh
+
+#include <cstdint>
+
+#include "geometry/distance.hpp"
+#include "geometry/point2d.hpp"
+#include "gtest/gtest.h"
+
+auto main(int32_t argc, char **argv) -> int32_t {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
