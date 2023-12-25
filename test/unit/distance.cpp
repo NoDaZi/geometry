c96ccb35f36a37b8a376d263c9c09dd9765274b9
Nodazi <dk3146@naver.com>
2023-12-25 15:38:22 +0900

    Skeleton code

---
+++ test/unit/distance.cpp
@@ -0,0 +1,23 @@
+/**
+ *@file geometry/unit/distance.cpp
+ *@author DaHyun Noh (dk3146@naver.com)
+ *@brief Point class developments with 2-dimension
+ *@version 1.0.0
+ *@date 2023-12-20
+ *@copyright Copyright (c) 2023 Programmers, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 Programmers, All Rights Reserved.
+// Authors : DaHyun Noh
+
+#include <cmath>
+#include <limits>
+#include <stdexcept>
+
+#include "gtest/gtest.h"
+
+namespace {
+constexpr uint32_t kTestCount = 10000U;
+}
+
+namespace nodazi::geometry {}
