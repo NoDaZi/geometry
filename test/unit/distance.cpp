ccc3d78d18ce08de0928bbcad5ed2e298a8618d5
Nodazi <dk3146@naver.com>
2023-12-30 17:03:49 +0900

    Added Test(GetValue,SetValue , Operators)

--- test/unit/distance.cpp
+++ test/unit/distance.cpp
@@ -29,9 +29,114 @@ TEST(GeometryDistance, Constructor) {
   Distance distance2(distance1);
   Distance distance3(std::move(Distance()));
 }
+
+TEST(GeometryDistance, ConstructorWithInputValue) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    Distance distance1(static_cast<double>(std::rand()));
+    Distance distance2(static_cast<double>(std::rand()),
+                       Distance::Type::kKilometer);
+    Distance distance3(static_cast<double>(std::rand()),
+                       Distance::Type::kMeter);
+    Distance distance4(static_cast<double>(std::rand()),
+                       Distance::Type::kCentimeter);
+    Distance distance5(static_cast<double>(std::rand()),
+                       Distance::Type::kMillimeter);
+    Distance distance6(static_cast<double>(std::rand()),
+                       Distance::Type::kMicrometer);
+    Distance distance7(static_cast<double>(std::rand()),
+                       Distance::Type::kNanometer);
+  }
+}
+
 TEST(GeometryPoint2D, AssignmentOperator) {
   Distance Distance1;
   auto Distance2 = Distance1;
   auto Distance3 = std::move(Distance());
 }
+
+TEST(GeometryDistance, GetValue) {
+  const auto kInputValue = static_cast<double>(2038.0);
+
+  Distance distance1(kInputValue, Distance::Type::kKilometer);
+  EXPECT_DOUBLE_EQ(kInputValue, distance1.GetValue(Distance::Type::kKilometer));
+
+  Distance distance2(kInputValue, Distance::Type::kMeter);
+  EXPECT_DOUBLE_EQ(kInputValue, distance2.GetValue(Distance::Type::kMeter));
+
+  Distance distance3(kInputValue, Distance::Type::kCentimeter);
+  EXPECT_DOUBLE_EQ(kInputValue,
+                   distance3.GetValue(Distance::Type::kCentimeter));
+
+  Distance distance4(kInputValue, Distance::Type::kMillimeter);
+  EXPECT_DOUBLE_EQ(kInputValue,
+                   distance4.GetValue(Distance::Type::kMillimeter));
+
+  Distance distance5(kInputValue, Distance::Type::kMicrometer);
+  EXPECT_DOUBLE_EQ(kInputValue,
+                   distance5.GetValue(Distance::Type::kMicrometer));
+
+  Distance distance6(kInputValue, Distance::Type::kNanometer);
+  EXPECT_DOUBLE_EQ(kInputValue, distance6.GetValue(Distance::Type::kNanometer));
+}
+
+TEST(GeometryDistance, SetValue) {
+  const auto kInputValue = static_cast<double>(2038.0);
+
+  Distance distance1;
+  distance1.SetValue(kInputValue, Distance::Type::kKilometer);
+  EXPECT_DOUBLE_EQ(kInputValue, distance1.GetValue(Distance::Type::kKilometer));
+
+  Distance distance2;
+  distance2.SetValue(kInputValue, Distance::Type::kMeter);
+  EXPECT_DOUBLE_EQ(kInputValue, distance2.GetValue(Distance::Type::kMeter));
+
+  Distance distance3;
+  distance3.SetValue(kInputValue, Distance::Type::kCentimeter);
+  EXPECT_DOUBLE_EQ(kInputValue,
+                   distance3.GetValue(Distance::Type::kCentimeter));
+
+  Distance distance4;
+  distance4.SetValue(kInputValue, Distance::Type::kMillimeter);
+  EXPECT_DOUBLE_EQ(kInputValue,
+                   distance4.GetValue(Distance::Type::kMillimeter));
+
+  Distance distance5;
+  distance5.SetValue(kInputValue, Distance::Type::kMicrometer);
+  EXPECT_DOUBLE_EQ(kInputValue,
+                   distance5.GetValue(Distance::Type::kMicrometer));
+
+  Distance distance6;
+  distance6.SetValue(kInputValue, Distance::Type::kNanometer);
+  EXPECT_DOUBLE_EQ(kInputValue, distance6.GetValue(Distance::Type::kNanometer));
+}
+
+TEST(GeometryDistance, OperatorEqual) {
+  const auto kInputValue = static_cast<double>(2038.0);
+  Distance distance_by_kilo(kInputValue, Distance::Type::kKilometer);
+  Distance distance(kInputValue * 1.0e+3, Distance::Type::kMeter);
+  Distance distance_by_nano(kInputValue * 1.0e+12, Distance::Type::kNanometer);
+
+  EXPECT_TRUE(distance_by_kilo == distance);
+  EXPECT_TRUE(distance_by_kilo == distance_by_nano);
+  EXPECT_TRUE(distance_by_nano == distance);
+}
+
+TEST(GeometryDistance, OperatorPlus) {
+  const auto kInputValue = static_cast<double>(2038.0);
+  Distance distance_by_kilo(kInputValue, Distance::Type::kKilometer);
+  Distance distance(kInputValue * 1.0e+3, Distance::Type::kMeter);
+  Distance distance_by_nano(kInputValue * 1.0e+12, Distance::Type::kNanometer);
+
+  const auto distance1 = distance_by_kilo + distance;
+  const auto distance2 = distance_by_kilo + distance_by_nano;
+  const auto distance3 = distance + distance_by_nano;
+
+  EXPECT_DOUBLE_EQ(distance1.GetValue(Distance::Type::kKilometer),
+                   kInputValue * 2.0);
+  EXPECT_DOUBLE_EQ(distance2.GetValue(Distance::Type::kKilometer),
+                   kInputValue * 2.0);
+  EXPECT_DOUBLE_EQ(distance3.GetValue(Distance::Type::kKilometer),
+                   kInputValue * 2.0);
+}
+
 }  // namespace nodazi::geometry
